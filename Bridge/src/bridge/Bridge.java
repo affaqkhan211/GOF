e3e496535105e2b1928be4ef29dca46a036f5424
affaqkhan211 <affaqkhan211@gmail.com>
2023-12-22 09:51:53 +0500

    bridge and factory patterns

---
+++ Bridge/src/bridge/Bridge.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridge;
+
+/**
+ *
+ * @author fa20-bse-057
+ */
+public class Bridge {
+   public static void main(String[] args) {
+      Shape redCircle = new Circle(100,100, 10, new RedCircle());
+      Shape greenCircle = new Circle(100,100, 10, new GreenCircle());
+
+      redCircle.draw();
+      greenCircle.draw();
+   }
+}
---
+++ Bridge/src/bridge/DrawAPI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridge;
+
+/**
+ *
+ * @author fa20-bse-057
+ */
+public interface DrawAPI {
+   public void drawCircle(int radius, int x, int y);
+}
